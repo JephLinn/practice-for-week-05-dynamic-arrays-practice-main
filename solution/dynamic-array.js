dd055670405a89f4a7cc5dc4753f42b161519d4f
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-11-06 18:47:51 -0500

    Done

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -2,18 +2,32 @@ class DynamicArray {
 
   constructor(defaultSize=4) {
 
-    // Your code here
+    defaultSize = defaultSize || 4;
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
 
   }
 
   read(index) {
-
-    // Your code here
+    return this.data[index];
+    
   }
 
   unshift(val) {
 
-    // Your code here
+    let newLength = this.length + 1;
+
+    if (newLength > this.capacity) {
+      this.resize();
+    }
+
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+
+    this.data[0] = val;
+    this.length = newLength;
   }
 
 }
